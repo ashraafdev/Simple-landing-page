3e7ed7b23d43f79ba745a2f8048c7ac582b09405
MrHraf <87606028+MrHraf@users.noreply.github.com>
2021-08-29 23:02:45 +0100

    Update deleteUser.php

--- Administrator/files/iframes/deleteUser.php
+++ Administrator/files/iframes/deleteUser.php
@@ -5,18 +5,20 @@
 
   if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    
+  } else {
+    
+    $sql_request = "DELETE FROM admin WHERE username='$usrname'";
+
+    if(mysqli_query($db, $sql_request)) {
+      
+      echo "<script>alert('Done');</script>";
+      mysqli_close($db);
+      echo "<script>location.href='admins-management.php';</script>";
+
+    } else{
+      echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    }
   }
-
-  $sql_request = "DELETE FROM admin WHERE username='$usrname'";
-
-  if(mysqli_query($db, $sql_request)) {
-
-    echo "<script>alert('Done');</script>";
-    mysqli_close($db);
-    echo "<script>location.href='admins-management.php';</script>";
-
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
 
 ?>
